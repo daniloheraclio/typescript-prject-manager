b6ae8a0a264cda93a8b7e3f9c3f2a0cf2839eebc
Danilo Heraclio <emaildedanilo@gmail.com>
2023-02-25 14:12:58 -0500

    drag Events & reflecting the current state in te UI

--- src/app.ts
+++ src/app.ts
@@ -196,7 +196,7 @@ class ProjectItem
 }
 
 // ProjectList Class
-class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
   assignProjects: Project[] = [];
 
   constructor(private type: 'active' | 'finished') {
@@ -207,7 +207,25 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     this.renderContent();
   }
 
+  @autobind
+  dragOverHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.add('droppable');
+  }
+
+  dropHandler(_: DragEvent) {}
+
+  @autobind
+  dragLeaveHandler(_: DragEvent) {
+    const listEl = this.element.querySelector('ul')!;
+    listEl.classList.remove('droppable');
+  }
+
   configure() {
+    this.element.addEventListener('dragover', this.dragOverHandler);
+    this.element.addEventListener('dragleave', this.dragLeaveHandler);
+    this.element.addEventListener('drop', this.dropHandler);
+
     projectState.addListener((projects: Project[]) => {
       const relevantProjects = projects.filter(({ status }) => {
         if (this.type === 'active') {
